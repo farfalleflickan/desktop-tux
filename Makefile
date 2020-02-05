69852dd4d553847b06ba1fd11a9f0147df39e1ff
pastapojken <pastapojken@users.noreply.github.com>
2020-02-05 09:20:39 +0100

    Added video of current status/functionality

--- Makefile
+++
@@ -1,8 +0,0 @@
-CFLAGS=-O2 -Wall -lX11 -lXi -lXpm -lXext
-
-.PHONY : all
-all:
-	${CC} ${CFLAGS} *.c -o main
-	
-run : all
-	@exec ./main
--- event.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <X11/XKBlib.h>
-#include <X11/extensions/XInput2.h>
-#include "display.h"
-
-void print_rawEvent(XIRawEvent *event);
-int inputLoop(display *myDisplay);
-
